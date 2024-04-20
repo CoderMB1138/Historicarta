374ee73247db906344b3d93141ee4ff5e7a4380d
CoderMB1138 <mbagdazian90@gmail.com>
2024-04-20 19:48:32 +0200

    Map started!

--- historicarta_frontend/src/App.js
+++ historicarta_frontend/src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { WorldMap } from "react-svg-worldmap";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const data = [
+  { "country": "cn", value: 5 }, // china
+  { "country": "us", value: 10 },  // united states
+  { "country": "ru", value: 7 },  // russia
+];
+
+const stylingFunction = (context) => {
+  const opacityLevel = 0.1 + (1.5 * (context.countryValue - context.minValue) / (context.maxValue - context.minValue))
+  return { fill: context.country === "US" ? "blue" : context.color, fillOpacity: opacityLevel, stroke: "green", strokeWidth: 1, strokeOpacity: 0.2, cursor: "pointer" }
+}
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="Main">
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <WorldMap color={"red"} type={"marker"} tooltipBgColor={"#D3D3D3"} title="Historicarta" valueSuffix="points" size="lg" data={data} frame={true} styleFunction={stylingFunction}/>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
+
